2d9acd0de5c64cc1b12c94ee82879a288de231be
mairaalvs <mairaalves@gec.inatel.br>
2022-10-28 09:34:43 -0300

    Criação do projeto

---
+++ src/main/java/br/com/inatel/quotationmanagement/QuotationmanagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.inatel.quotationmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotationmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotationmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/inatel/quotationmanagement/QuotationmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.inatel.quotationmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotationmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
